ea2de4093b88a6d3b75f6d6584c9e3d15410c89a
JheyBi <joaodlrio@outlook.com>
2023-07-31 11:39:25 -0300

    novos Tokens

--- ETokenType.cs
+++ ETokenType.cs
@@ -7,6 +7,11 @@ namespace Translator {
         DIV = 4,
         OPEN = 5,
         CLOSE = 6,
+        AT = 7,
+        VAR = 8,
+        OUTPUT = 9,
+        INPUT = 10,
+        EOL = 11,
         EOF = -1,
     }
 }
