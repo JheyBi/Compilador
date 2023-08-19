0f6e21c5c6a1f625be6f7a75e8c3a297bc93d78a
lucasz <lucaschavesgeneroso@gmail.com>
2023-08-19 17:12:25 -0300

    Tratamento de exceções

--- Parser/Sintatic.cs
+++ Parser/Sintatic.cs
@@ -9,7 +9,13 @@ namespace Translator {
 
         public Sintatic(Lexer lexer, SymbolTable st){
             _lexer = lexer;
-            _LA = lexer.GetNextToken();
+            try {
+                _LA = lexer.GetNextToken();
+            }
+            catch (InvalidChar ex) {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
             _symbolTable = st;
         }
 
@@ -35,10 +41,16 @@ namespace Translator {
              //Console.WriteLine("Token: " + token);
             if(_LA.Type == token){
                 // //Console.WriteLine("Token: " + _LA + " reconhecido");
-                _LA = _lexer.GetNextToken();
+                try {
+                    _LA = _lexer.GetNextToken();
+                }
+                catch (InvalidChar ex) {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 // //Console.WriteLine("Proximo Token: " + _LA + " a ser analisado");
-            }else{
-                throw new Exception("Erro de sintaxe");
+            } else {
+                throw new SyntaxError(_lexer.Line, "sintaxe, token não é o esperado");
             }
         }
 
@@ -62,13 +74,26 @@ namespace Translator {
         }
 
         public void Line(){ // Line: stmt EOL | EOF
-            Stmt();
+            try {
+                Stmt();
+            }
+            catch(SyntaxError ex){
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+            
             //Console.WriteLine("Enviando o match do EOL");
             if(_LA.Type == ETokenType.EOF){
                 Match(ETokenType.EOF);
             }
             else{
+                try{
                 Match(ETokenType.EOL);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
             }
         }
 
@@ -76,13 +101,19 @@ namespace Translator {
             //Console.WriteLine("----------------");
             //Console.WriteLine("LA no A: " + _LA);
             if(_LA.Type==ETokenType.EOF){
-                Match(ETokenType.EOF);
+                try{
+                    Match(ETokenType.EOF);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
             }
             // else if(_LA.Type==ETokenType.EOL){
             //     Match(ETokenType.EOL);
             //     A();
             // }
-            else{
+            else {
                 Prog();
             }
         }
@@ -97,16 +128,27 @@ namespace Translator {
             else if(_LA.Type==ETokenType.VAR){
                 Atrib();
             }
-            else{
-                throw new Exception("ERRO de sintaxe: " + _LA.Type + " não esperado, esperava INPUT, OUTPUT ou VAR");
+            else {
+                throw new SyntaxError(_lexer.Line, "não esperado, esperava INPUT, OUTPUT ou VAR",_LA.Type.ToString());
             }
         }
 
         public void In(){ //in: INPUT VAR
-            
-            Match(ETokenType.INPUT);
+            try{
+                Match(ETokenType.INPUT);
+            }
+            catch(SyntaxError ex){
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
             var refx = _LA.Value;
-            Match(ETokenType.VAR);
+            try{
+                Match(ETokenType.VAR);
+            }
+            catch(SyntaxError ex){
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
             Console.WriteLine("Digite um valor: ");
             //Pegar o valor inteiro digitado
             var valor = Convert.ToInt32(Console.ReadLine());
@@ -120,17 +162,40 @@ namespace Translator {
         }
 
         public void Out(){ //out    : OUTPUT VAR
-            Match(ETokenType.OUTPUT);
+            try{
+                Match(ETokenType.OUTPUT);
+            }
+            catch(SyntaxError ex){
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
             var refx = _LA.Value;
-            Match(ETokenType.VAR);
+            try{
+                Match(ETokenType.VAR);
+            }
+            catch(SyntaxError ex){
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
             Console.WriteLine("Resultado: " + _symbolTable.GetEntry(refx).Value);
         }
 
         public void Atrib(){//atrib  : VAR AT expr
             var refx = _LA.Value;
-            Console.WriteLine("LA do atrib: " + _LA.Value);
-            Match(ETokenType.VAR);
-            Match(ETokenType.AT);
+            try{
+                Match(ETokenType.VAR);
+            }
+            catch(SyntaxError ex){
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+            try{
+                Match(ETokenType.AT);
+            }
+            catch(SyntaxError ex){
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
             var res = E();
             //Colocar a variavel na tabela
             if (refx != null){
@@ -152,13 +217,25 @@ namespace Translator {
         public int X(int t){ //X      : vazio | + expr | - expr
             ////Console.WriteLine("Estado: X ->  LA:" + _LA.Value);
             if(_LA.Type==ETokenType.SUM){
-                Match(ETokenType.SUM);
+                try{
+                    Match(ETokenType.SUM);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 var res = E();
                 ////Console.WriteLine("Estou no X(+) e t é: " + t + " e E é: " + res);
                 return t + res;
             }
             else if(_LA.Type==ETokenType.SUB){
-                Match(ETokenType.SUB);
+                try{
+                    Match(ETokenType.SUB);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 var res = E();
                 ////Console.WriteLine("Estou no X(-) e t é: " + t + " e E é: " + res);
                 return t - res;
@@ -183,15 +260,26 @@ namespace Translator {
         public int Y(int t){ // Y     : vazio | * term | / term
             ////Console.WriteLine("Estado: Y ->  LA:" + _LA.Value);
             if(_LA.Type==ETokenType.MUL){
-                
-                Match(ETokenType.MUL);
+                try{
+                    Match(ETokenType.MUL);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 var res = T();
                 ////Console.WriteLine("Estou no Y(*) e t é: " + t + " e E é: " + res);
                 return t * res;
 
             }
             else if(_LA.Type==ETokenType.DIV){
-                Match(ETokenType.DIV);
+                try{
+                    Match(ETokenType.DIV);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 var res = T();
                 ////Console.WriteLine("Estou no X(/) e t é: " + t + " e E é: " + res);
                 return t / res;
@@ -208,14 +296,32 @@ namespace Translator {
             //Console.WriteLine("Estado: F ->  LA:" + _LA.Value);
             if(_LA.Type==ETokenType.NUMBER){
                 var res = _LA.Value;
+                try{
                 Match(ETokenType.NUMBER);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 ////Console.WriteLine("Estou no F e LA é: " + res);
                 return res;
             }
             else if(_LA.Type==ETokenType.OPEN){
-                Match(ETokenType.OPEN);
+                try{
+                    Match(ETokenType.OPEN);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 var res = E();
-                Match(ETokenType.CLOSE);
+                try{
+                    Match(ETokenType.CLOSE);
+                }
+                catch(SyntaxError ex){
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
                 ////Console.WriteLine("Estou no F e E é: " + res);
                 return res;
             }
@@ -226,6 +332,7 @@ namespace Translator {
             }
             return 0;
         }
+
     }
 }
 
